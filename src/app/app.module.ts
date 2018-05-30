317f7a5188b04cfbdf7f56e28cf457f8ac067406
Meet Shah <meetdshah007@gmail.com>
2018-05-30 23:41:20 +0530

    First commit before deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
+import { OrderComponent } from './order/order.component';
+import { SummaryComponent } from './summary/summary.component';
+import { MaterialModule } from './material.module';
+
+const appRoutes: Routes = [
+  { path: 'order', component: OrderComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: '', component: DashComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
+    OrderComponent,
+    SummaryComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
